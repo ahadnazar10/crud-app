95541aaa9c241cab5d0e01224cb91ec08ae101f6
ahadnazar10 <ahad.adam08@gmail.com>
2025-07-20 02:30:31 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,18 @@ const app = express();
 app.listen(8800, () => {
   console.log("Connected to backend.");
 });
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
